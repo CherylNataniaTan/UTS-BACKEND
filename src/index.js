0932c161fcd84d797604e35e9687365116ac9080
CherylNataniaTan <cnataniatan@gmail.com>
2026-04-20 00:04:11 +0700

    cheryl

---
+++ src/index.js
@@ -0,0 +1,19 @@
+require('dotenv').config() 
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(express.json());
+
+const routes = require("./routes/routes");
+
+app.use("/", routes);
+
+mongoose.connect("mongodb://127.0.0.1:27017/digitalBanking")
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+app.listen(5000, () => {
+  console.log("Server jalan di port 5000");
+});
