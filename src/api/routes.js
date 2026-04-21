2db3f67fb36793a43edaad7f320d2fd4b69d6c4e
frengky <frengkydoang01@gmail.com>
2026-04-21 11:51:56 +0700

    fix update

--- src/api/routes.js
+++ src/api/routes.js
@@ -1,7 +1,18 @@
+<<<<<<< HEAD
 const express = require("express");
 const router = express.Router();
 
 // const transactionsRoutes = require("./api/components/transactions/transactions-route");
 // router.use("/histories", historiesRoute);
+=======
+const express = require('express');
+const router = express.Router();
+
+const historiesRoute = require('./components/histories/histories-route')
+const topupsRoute = require('./components/topups/topups-route');
+
+router.use(historiesRoute);
+router.use(topupsRoute);
+>>>>>>> 7a554114880febfd7d4f128f92e7f2add859d2f6
 
 module.exports = router;
